1c46204681c7fe48a2d1b3e811cc9875fdf12ad3
Amir Abdullah <amir@cynch.ai>
2025-01-26 19:10:11 -0700

    Add value.

--- sae/sae_interp.py
+++ sae/sae_interp.py
@@ -519,7 +519,7 @@ class LoadedSAES:
         Converts transformer.h[0].mlp into self.language_model.transformer.h[0].mlp.output.save() for nnsight
         """
         subbed_layer = re.sub(r'\.([0-9]+)(?=\.|$)', r'[\1]', layer)
-        return f"self.language_model.{subbed_layer}.output.save().value"
+        return f"self.language_model.{subbed_layer}.output.save()"
 
     def encode_to_activations_for_layer(self, text: str, layer: str):
         with torch.no_grad():
@@ -529,9 +529,9 @@ class LoadedSAES:
                     my_output = eval(eval_string)
                     print(f"Output is my_output {my_output} for eval_string {eval_string}")
         if len(my_output) > 1 or isinstance(my_output, tuple):
-            return my_output[0]
+            return my_output.value[0]
         else:
-            return my_output
+            return my_output.value
 
     def tokenize_to_max_len(self, text, pad_to_max_seq_len):
         if pad_to_max_seq_len < 0:
