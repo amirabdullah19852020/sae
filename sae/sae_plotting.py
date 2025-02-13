f787cd1115cfc69ae4b47f6908173db6aac2e43b
Amir Abdullah <amir@cynch.ai>
2025-02-13 12:24:26 -0700

    Update scale for higher dpi.

--- sae/sae_plotting.py
+++ sae/sae_plotting.py
@@ -123,7 +123,7 @@ def plot_layer_features(full_layer_data, tag, model_name, top_n=20, cols=2):
     fig.update_xaxes(title_text="Feature Number", tickangle=90)
     fig.update_yaxes(title_text="Weight")
 
-    fig.write_image(f"SAE_top_features_for_{tag}_{model_name}.png")
+    fig.write_image(f"SAE_top_features_for_{tag}_{model_name}.png", scale=4)
 
     # Show the plot
     fig.show()
